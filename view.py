19087968389693d46dabc65760ee296b65390229
Sergei-Strelin <112539172+Sergei-Strelin@users.noreply.github.com>
2024-01-24 08:40:46 +0300

    Add files via upload

---
+++ view.py
@@ -0,0 +1,30 @@
+import sys
+
+import exceptions
+
+
+class View:
+    def __init__(self, game):
+        self.game = game
+
+    def start_game(self):
+        while True:
+            self.show_field()
+            player = self.game.get_active_player()
+            while True:
+                try:
+                    cell = int(input(f'Ход {player.item}: '))
+                    self.game.move(player, cell)
+                    break
+                except exceptions.EndGameException as ex:
+                    print('\n'*2 + str(ex))
+                    sys.exit()
+                except exceptions.GameException as ex:
+                    print(ex)
+
+    def show_field(self):
+        print('\n'*2 + '-' * 9)
+        for i in range(0, 9, 3):
+            dct = [*self.game.field.cells.items()][i: i + 3]
+            print('\t'.join([str(cell[0]) if cell[1].item is None else cell[1].item for cell in dct]))
+            print('-' * 9)
